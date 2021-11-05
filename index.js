dbff5ae27b2b871bbca77afd8f6be6194214083b
Lucas Templeton <templetl99@gmail.com>
2021-11-04 22:39:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('text').innerText = "This is really cool!"
+
+    console.log(document.getElementById('text'))
+});
